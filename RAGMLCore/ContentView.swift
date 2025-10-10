893bdd2ee20b556bc05bf80e1ebcbbc010408ad7
gunnarguy <gunnarguy@me.com>
2025-10-09 19:52:04 -0700

    Initial Commit

---
+++ RAGMLCore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RAGMLCore
+//
+//  Created by Gunnar Hostetler on 10/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RAGMLCore/RAGMLCoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  RAGMLCoreApp.swift
+//  RAGMLCore
+//
+//  Created by Gunnar Hostetler on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct RAGMLCoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
